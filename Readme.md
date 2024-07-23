988f0a3a197a337c251c72f7bd9496fcb97fa70a
Vedant Rajendra Shetye <97832138+NOOBPOOK@users.noreply.github.com>
2024-07-23 18:47:18 +0530

    Updated documentation

---
+++ Readme.md
@@ -0,0 +1,26 @@
+This code performs addition/subtraction of all possible base numbers.
+You can input numbers with base as
+2 - binary
+8 - octal
+16 - hexdecimal
+
+Then it gives step by step procedures to perform the required opeartion.
+There are many websites avaialbe which performs the following operation,
+but none of them gives you a detailed solution.
+
+This project can be a good starting point for understanding the basics of python, as there is a long way to go in the likes of Machine Learning, Data Science and much more. This project was created as a part of a mini-project for college and the topic was chosen as many people find difficulty in performing subtraction and 2's compliment concept.
+This type of operations are not used anymore and is just a serves as a good challenge to strengthen your basics of maths as well as code.
+
+Here are few examples->
+
+Positive answer with base 10
+![alt text](image.png)
+
+Negative answer with base 10
+![alt text](image-1.png)
+
+Positive answer with base 8
+![alt text](image-2.png)
+
+NOTE: Hexadecimal part is not 100% done and contain errors while doing subtraction part,
+Feel free to have a look and debug it! 😅
---
+++ add_subtract_all_bases.py
@@ -0,0 +1,301 @@
+# Program for using 2's compliment subtraction
+import textwrap
+from textwrap import wrap
+
+#Takes string as input and converts into binary bits from the concerned base
+def conv_binary(num, bas):
+    if "-" in num:
+        num = num.replace("-", "")
+    if "+" in num:
+        num = num.replace("+", "")
+    quo = []
+    rem = []
+    org = num
+
+    #If base is 10 (Decimal)
+    if bas == 10:
+        num = str(num).split(".")
+        while int(num[0]) != 0:
+            quo.append(str(int(num[0]) % 2))
+            num[0] = int(num[0]) // 2
+        if len(num) == 2:
+            jk = 0
+            dec = int(num[1]) / pow(10, len(num[1]))
+            while dec != 0 and jk <= 7:
+                dec = dec * 2
+                dec = str(dec).split(".")
+                rem.append(str(dec[0]))
+                dec = int(dec[1]) / pow(10, len(dec[1]))
+                jk += 1
+        org = org.split(".")
+        if len(org) == 1:
+            return ' '.join(textwrap.wrap(''.join(quo[::-1]).rjust(8, "0"), 4))
+        else:
+            return ''.join(quo[::-1]) + '.' + ''.join(rem)
+
+    #If base is 8 (Octal)
+    elif bas == 8:
+        octal = {0: '000', 1: '001', 2: '010', 3: '011', 4: '100', 5: '101', 6: '110', 7: '111'}
+        num = num.split(".")
+        for i in num[0]:
+            quo.append(octal[int(i)])
+        if len(num) == 2:
+            for j in num[1]:
+                quo.append(octal[int(j)])
+        return ' '.join(textwrap.wrap(''.join(quo).rjust(8, "0"), 4))
+
+    #If base is 16 (Hexadecimal)
+    elif bas == 16:
+        hexa = {'0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100', '5': '0101', '6': '0110', '7': '0111',
+                '8': '1000',
+                '9': '1001', 'A': '1010', 'B': '1011', 'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'}
+        num = num.split(".")
+        for i in num[0]:
+            quo.append(hexa[str(i)])
+        if len(num) == 2:
+            for j in num[1]:
+                quo.append(hexa[str(j)])
+        return ' '.join(quo)
+
+#Gets the complimentary of a Binary String
+def getComp(num, bin):
+    detect = False
+    st = ""
+    bina = list(bin)
+    for i in list(reversed(bina[1:9])):
+        if detect:
+            if i == "1":
+                st += "0"
+            elif i == "0":
+                st += "1"
+        else:
+            if i == "1":
+                st += "1"
+                detect = True
+            elif i == "0":
+                st += "0"
+    return ' '.join(textwrap.wrap("1" + ''.join(list(reversed(st))), 4))
+
+#Adds both the binary numbers as string input
+def add_bin(bt1, bt2):
+    bt1 = list(reversed(bt1))
+    bt2 = list(reversed(bt2))
+    add = ""
+    carry = False
+    for i in zip(bt1, bt2):
+        if carry is False:
+            if i[0] == "0":
+                if i[1] == "0":
+                    add += "0"
+                elif i[1] == "1":
+                    add += "1"
+            elif i[0] == "1":
+                if i[1] == "1":
+                    add += "0"
+                    carry = True
+                elif i[1] == "0":
+                    add += "1"
+        elif carry:
+            if i[0] == "0":
+                if i[1] == "0":
+                    add += "1"
+                    carry = False
+                elif i[1] == "1":
+                    add += "0"
+                    carry = True
+            elif i[0] == "1":
+                if i[1] == "1":
+                    add += "1"
+                    carry = True
+                elif i[1] == "0":
+                    add += "0"
+                    carry = True
+    if carry:
+        add += "1"
+    return add[::-1]
+
+#This function is used for postprocessing to get final answer and map correctly into correct base as input
+def pos(add, bass):
+    add = add.replace("1",'',1)
+    if add.startswith("1"):
+        print(f"\nCarry is generated. Discarding carry we get,\n    {add}")
+        print("Since magnitude is 1, the answer is negative and in 2's Compliment,")
+        neg(add, base)
+    else:
+        add.replace('1','',1)
+        print(
+            "\nCarry is generated. Discarding carry we get,\n" + "  + " + ' '.join(textwrap.wrap(add, 4)))
+        if bass == 10:
+            print(f"Since it is in decimal,")
+            tot1 = 0
+            x = 0
+            for i in list(reversed(add)):
+                tot1 += int(i)*(2**x)
+                x += 1
+            print(f"\n*****The final answer is {tot1} to base 10.*****")
+        elif bass == 8:
+            octal = {'000': '0', '001': '1', '010': '2', '011': '3', '100': '4', '101': '5', '110': '6', '111': '7'}
+            print("For Octal conversion, grouping into 3, we get,")
+            add = textwrap.wrap(add.rjust(((len(add) // 3) + 1) * 3, "0"), 3)
+            print("    " + ' '.join(add))
+            final_ans = ""
+            for i in add:
+                final_ans += octal[i]
+            print(f"\n*****The final answer is ({final_ans}) to base 8.*****")
+        elif bass == 16:
+            hexa = {'0000': '0', '0001': '1', '0010': '2', '0011': '3', '0100': '4', '0101': '5', '0110': '6', '0111': '7',
+                    '1000': '8',
+                    '1001': '9', '1010': 'A', '1011': 'B', '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'}
+            print("For Hexadecimal conversion, grouping into 4, we get,")
+            add = textwrap.wrap(add, 4)
+            print("    " + ' '.join(add))
+            final_ans = ""
+            for i in add:
+                final_ans += hexa[i]
+            print(f"\n*****The final answer is ({final_ans}) to base 16.*****")
+
+#This function is used for postprocessing to get final answer and map correctly into correct base as input
+def neg(add, bass):
+    detect = False
+    st = ""
+    add = list(add)
+    for i in list(reversed(add[0:8])):
+        if detect:
+            if i == "1":
+                st += "0"
+            elif i == "0":
+                st += "1"
+        else:
+            if i == "1":
+                st += "1"
+                detect = True
+            elif i == "0":
+                st += "0"
+    add = ''.join(list(reversed(st)))
+    print("    " + add)
+    if bass == 10:
+        print(f"\nSince it is in decimal,")
+        tot1 = 0
+        x = 0
+        for i in list(reversed(add)):
+            tot1 += int(i) * (2 ** x)
+            x += 1
+        print(f"*****The final answer is -{tot1} to base 10.*****")
+    elif bass == 8:
+        octal = {'000': '0', '001': '1', '010': '2', '011': '3', '100': '4', '101': '5', '110': '6', '111': '7'}
+        print("For Octal conversion, grouping into 3, we get,")
+        add = textwrap.wrap(add.rjust(((len(add) // 3) + 1) * 3, "0"), 3)
+        print("    " + ' '.join(add))
+        final_ans = ""
+        for i in add:
+            final_ans += octal[i]
+        print(f"\n*****The final answer is -({final_ans}) to base 8.******")
+    elif bass == 16:
+        hexa = {'0000': '0', '0001': '1', '0010': '2', '0011': '3', '0100': '4', '0101': '5', '0110': '6', '0111': '7',
+                '1000': '8',
+                '1001': '9', '1010': 'A', '1011': 'B', '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'}
+        print("For Hexadecimal conversion, grouping into 4, we get,")
+        add = textwrap.wrap(add, 4)
+        print("    " + ' '.join(add))
+        final_ans = ""
+        for i in add:
+            final_ans += hexa[i]
+        print(f"\n*****The final answer is -({final_ans}) to base 16.*****")
+
+#This function is used to add both the numbers as they are positive
+def only_add(t1, t2, bass):
+    print("\nNow for the addition part,")
+    add = add_bin(t1, t2)
+    print(' '.join(textwrap.wrap(''.join(t1), 1)).rjust(20, " "))
+    print(' '.join(textwrap.wrap("+ " + ''.join(t2), 1)).rjust(20, " "))
+    print(("-" * 17).rjust(20, " "))
+    print(' '.join(textwrap.wrap(add, 1)).rjust(20, " "))
+    if bass == 10:
+        print(f"Since it is in decimal,")
+        tot1 = 0
+        x = 0
+        for i in list(reversed(add)):
+            tot1 += int(i)*(2**x)
+            x += 1
+        print(f"\n*****The final answer is {tot1} to base 10.*****")
+    elif bass == 8:
+        octal = {'000': '0', '001': '1', '010': '2', '011': '3', '100': '4', '101': '5', '110': '6', '111': '7'}
+        print("For Octal conversion, grouping into 3, we get,")
+        add = textwrap.wrap(add.rjust(((len(add) // 3) + 1) * 3, "0"), 3)
+        print("    " + ' '.join(add))
+        final_ans = ""
+        for i in add:
+            final_ans += octal[i]
+        print(f"\n*****The final answer is ({final_ans}) to base 8.*****")
+    elif bass == 16:
+        hexa = {'0000': '0', '0001': '1', '0010': '2', '0011': '3', '0100': '4', '0101': '5', '0110': '6', '0111': '7',
+                '1000': '8',
+                '1001': '9', '1010': 'A', '1011': 'B', '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'}
+        print("For Hexadecimal conversion, grouping into 4, we get,")
+        add = textwrap.wrap(add.rjust(((len(add) // 4) + 1) * 4, "0"), 4)
+        print("    " + ' '.join(add))
+        final_ans = ""
+        for i in add:
+            final_ans += hexa[i]
+        print(f"\n*****The final answer is ({final_ans}) to base 16.*****")
+
+"""
+This part takes input into the program
+Example 
+23 -49 -> base10
+14 65 ->base8
+A3 -99 ->base16
+""" 
+#Takes th 2 numbers to work on as input
+print("Enter the expression!")
+n = list(map(str, input().split(" ")))
+og = n
+
+#Enter the base of both the numbers
+print("Enter the base of both the numbers!")
+base = int(input())
+
+#Starting the Solution & Printing
+print("\n" + "*" * 20 + "SOLUTION" + "*" * 20)
+
+#Converting the first number into binary
+print(f"\nFor first number {n[0]} we get,")
+bt1 = conv_binary(n[0], base)
+print(n[0].rjust(4, " ") + ": " + bt1 + " " * 4 + "<---8bit")
+
+#Converting the second number into binary
+print(f"\nFor second number {n[1]} we get,")
+bt2 = conv_binary(n[1], base)
+print(n[1].rjust(4, " ") + ": " + bt2 + " " * 4 + "<---8bit")
+
+#If both the numbers are positive simply add the numbers
+if "-" not in og[0] and "-" not in og[1]:
+    only_add(bt1, bt2, base)
+else:
+    #Getting 2's compliment for the concerned negative numbers
+    print("\nGetting 2's Compliments for Negative Numbers")
+    if "-" in og[0]:
+        bt1 = getComp(og[0], bt1)
+        print(og[0].rjust(4, " ") + ": " + bt1 + " " * 4 + "<---2's Compliment MS-bit")
+    if "-" in og[1]:
+        bt2 = getComp(og[1], bt2)
+        print(og[1].rjust(4, " ") + ": " + bt2 + " " * 4 + "<---2's Compliment MS-bit")
+    
+    #Now adding the compliments to get the desired result
+    print("\nNow for the addition part,")
+    bt1 = bt1.replace(" ", "")
+    bt2 = bt2.replace(" ", "")
+    print(' '.join(textwrap.wrap(''.join(bt1), 1)).rjust(20, " "))
+    print(' '.join(textwrap.wrap("+ " + ''.join(bt2), 1)).rjust(20, " "))
+    print(("-" * 17).rjust(20, " "))
+    suma = add_bin(bt1, bt2)
+
+    #Printing final answer and determining if answer is positive or negative
+    print(' '.join(textwrap.wrap(suma, 1)).rjust(20, " "))
+    if len(suma) == 9:
+        pos(suma, base)
+    elif len(suma) == 8:
+        print("\nCarry is not generated. Answer is negative and in 2's Compliment,\n" + "    " + ' '.join(
+            textwrap.wrap(suma, 4)))
+        print("Converting the number in original form,")
+        neg(suma, base)
